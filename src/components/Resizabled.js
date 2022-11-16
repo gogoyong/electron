5381d0aa45411fe1227066615f4aab6edba4f01a
gogoyong <ook917@gmail.com>
2022-11-16 16:49:10 +0900

    chore: Resizabled 컴포넌트 수정

--- src/components/Resizabled.js
+++ src/components/Resizabled.js
@@ -1,24 +1,17 @@
 import * as React from 'react';
 import { Resizable } from 're-resizable';
 const style = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
   border: 'solid 0px #ddd',
 };
 
 const Resizabled = () => {
-  const [width, setWidth] = React.useState(300);
-  const [height, setHeight] = React.useState(1);
   return (
     <Resizable
       style={style}
-      size={{ width, height }}
-      onResizeStop={(e, direction, ref, d) => {
-        setWidth(width + d.width);
-        setHeight(height + d.height);
+      Size={{
+        width: 230,
       }}
-    ></Resizable>
+    />
   );
 };
 
