45457b38ba75b0f3a9b76207f7ab48e25f957539
gogoyong <ook917@gmail.com>
2022-11-17 17:56:30 +0900

    chore : index.html, main.js 내용 일부 수정

--- src/function.js
+++ src/function.js
@@ -1,16 +1,16 @@
-const { ipcRenderer } = require('electron');
-const ipc = ipcRenderer;
+// const { ipcRenderer } = require('electron');
+// const ipc = ipcRenderer;
 
-var btnMin = document.getElementById('min');
-var btnMax = document.getElementById('max');
-var btnClose = document.getElementById('close');
+// const btnMin = document.getElementById('min');
+// const btnMax = document.getElementById('max');
+// const btnClose = document.getElementById('close');
 
-btnMin.addEventListener('click', () => {
-  ipc.send('minimizeApp');
-});
-btnMax.addEventListener('click', () => {
-  ipc.send('maximizeApp');
-});
-btnClose.addEventListener('click', () => {
-  ipc.send('closeApp');
-});
+// btnMin.addEventListener('click', () => {
+//   ipc.send('minimizeApp');
+// });
+// btnMax.addEventListener('click', () => {
+//   ipc.send('maximizeApp');
+// });
+// btnClose.addEventListener('click', () => {
+//   ipc.send('closeApp');
+// });
